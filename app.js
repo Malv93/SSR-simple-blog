a24f0064487870534b6050b47942177f7697dd26
Marco Malvicini <marco.malvicini31@gmail.com>
2021-05-03 10:45:18 +0200

    Change env variable name.

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URL_TEST, {
+mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
